60ea9a77f1482c21409f685803f6eb887651e55a
rafsaf <rafal.safin12@gmail.com>
2021-01-18 22:52:56 +0100

    added diffrent handle for less and more than 25 points, fixed mistypes

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const PointsForm = ({ setPoints }) => {
         points: Yup.number()
           .positive("Ilość punktów musi być dodatnia")
           .integer("Liczba musi być całkowita")
+          .min(5,"Minimum 5 punktów")
           .required("Pole wymagane"),
       })}
       onSubmit={(values, { setSubmitting }) => {
@@ -24,7 +25,7 @@ const PointsForm = ({ setPoints }) => {
         <Form>
           <div>
             <BootstrapForm.Label>
-              Wpisz maks. ilość punktów ze sprawdzianu
+              Wpisz maks. ilość punktów ze sprawdzianu, dla 25 i więcej plusy i minusy przy ocenach.
             </BootstrapForm.Label>
           </div>
           <div className="row">
@@ -50,137 +51,181 @@ const PointsForm = ({ setPoints }) => {
   );
 };
 
-// markup
+const grades = [
+  "1",
+  "1+",
+  "2-",
+  "2",
+  "2+",
+  "3-",
+  "3",
+  "3+",
+  "4-",
+  "4",
+  "4+",
+  "5-",
+  "5",
+]
+
+const gradesMin = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+]
+
 const IndexPage = () => {
   const [points, setPoints] = useState(null);
 
   const submitPoints = (points) => {
     setPoints(points);
   };
+  const calculatePointsDown25 = () => {
+    const numbs = {
+      one: 0,
+      two: Math.ceil((points * 30) / 100),
+      three: Math.ceil((points * 50) / 100),
+      four: Math.ceil((points * 70) / 100),
+      five: Math.ceil((points * 90) / 100),
+    };
 
+    return [
+      [numbs.one, numbs.two - 1],
+      [numbs.two, numbs.three -1],
+      [numbs.three, numbs.four -1],
+      [numbs.four, numbs.five -1],
+      [numbs.five, points],
+    ]
+  }
+
+  const calculatePointsUp25 = () => {
+    const numbs = {
+      one: { down: 0, up: Math.round((points * 25) / 100) },
+      two: {
+        down: Math.round((points * 35) / 100),
+        up: Math.round((points * 45) / 100),
+      },
+      three: {
+        down: Math.round((points * 55) / 100),
+        up: Math.round((points * 65) / 100),
+      },
+      four: {
+        down: Math.round((points * 75) / 100),
+        up: Math.round((points * 85) / 100),
+      },
+      five: {
+        down: Math.round((points * 95) / 100),
+        up: Math.round((points * 100) / 100),
+      },
+    };
+    const diff_1 =
+      Math.round((points * 35) / 100) - Math.round((points * 25) / 100) - 1;
+    const diff_2 =
+      Math.round((points * 55) / 100) - Math.round((points * 45) / 100) - 1;
+    const diff_3 =
+      Math.round((points * 75) / 100) - Math.round((points * 65) / 100) - 1;
+    const diff_4 =
+      Math.round((points * 95) / 100) - Math.round((points * 85) / 100) - 1;
+
+    const up_one = numbs.one.up + Math.round(diff_1 / 2);
+    
+    const up_two = numbs.two.up + Math.round(diff_2 / 2);
+
+    const up_three = numbs.three.up + Math.round(diff_3 / 2);
+
+    const up_four = numbs.four.up + Math.round(diff_4 / 2);
+
+    return [
+      // 1
+      [numbs.one.down,
+      numbs.one.up],
+      // 1+
+      [numbs.one.up + 1,
+      up_one],
+        // 2-
+      [up_one + 1,
+      numbs.two.down - 1],
+        // 2
+      [numbs.two.down,
+      numbs.two.up],
+        //
+      [numbs.two.up + 1,
+      up_two],
+
+      [up_two + 1,
+      numbs.three.down - 1],
+
+      [numbs.three.down,
+      numbs.three.up],
+
+      [numbs.three.up + 1,
+      up_three],
+
+      [up_three + 1,
+      numbs.four.down - 1],
+
+      [numbs.four.down,
+      numbs.four.up],
+
+      [numbs.four.up + 1,
+      up_four],
+
+      [up_four + 1,
+      numbs.five.down - 1],
+
+      [numbs.five.down,
+        numbs.five.up],
+
+    ]
+  };
   return (
     <div className="container my-2">
       <h1>Kalkulator progów</h1>
       <PointsForm setPoints={submitPoints} />
 
       {points ? (
-        <div className="row" style={{ fontSize: "large" }}>
+        <div className="row" style={{ fontSize: "" }}>
           <div className="col-auto">
             <h3>Sugerowane progi:</h3>
-            <p>
-              1 &nbsp;&nbsp; <b>0 - {Math.round((points * 20) / 100)}</b> pkt
-            </p>
-            <p>
-              1+ &nbsp;&nbsp;{" "}
+
+            {(points >= 25) ?
+            calculatePointsUp25(points).map((row, index) => (
+              <p>
+              {grades[index]} &nbsp;&nbsp;{" "}
               <b>
-                {Math.round((points * 20) / 100 + 1)} -{" "}
-                {Math.round((points * 30) / 100 - 1)}
+                {row[0]} - {row[1]}
               </b>{" "}
               pkt
             </p>
-            <p>
-              2- &nbsp;&nbsp;{" "}
+            )) : 
+            calculatePointsDown25(points).map((row, index) => (
+              <p>
+              {gradesMin[index]} &nbsp;&nbsp;{" "}
               <b>
-                {Math.round((points * 30) / 100)} -{" "}
-                {Math.round((points * 35) / 100)}
+                {row[0]} - {row[1]}
               </b>{" "}
               pkt
             </p>
-            <p>
-              2 &nbsp;&nbsp;{" "}
-              <b>
-                {Math.round((points * 35) / 100) + 1} -{" "}
-                {Math.round((points * 45) / 100)}
-              </b>{" "}
-              pkt
-            </p>
-            <p>
-              2+ &nbsp;&nbsp;{" "}
-              <b>
-                {Math.round((points * 45) / 100)} -{" "}
-                {Math.round((points * 50) / 100 - 1)}
-              </b>{" "}
-              pkt
-            </p>
-            <p>
-              3- &nbsp;&nbsp;{" "}
-              <b>
-                {Math.round((points * 50) / 100)} -{" "}
-                {Math.round((points * 55) / 100)}
-              </b>{" "}
-              pkt
-            </p>
-            <p>
-              3 &nbsp;&nbsp;{" "}
-              <b>
-                {Math.round((points * 55) / 100 + 1)} -{" "}
-                {Math.round((points * 65) / 100)}
-              </b>{" "}
-              pkt
-            </p>
-            <p>
-              3+ &nbsp;&nbsp;{" "}
-              <b>
-                {Math.round((points * 65) / 100)} -{" "}
-                {Math.round((points * 70) / 100 - 1)}
-              </b>{" "}
-              pkt
-            </p>
-            <p>
-              4- &nbsp;&nbsp;{" "}
-              <b>
-                {Math.round((points * 70) / 100)} -{" "}
-                {Math.round((points * 75) / 100)}
-              </b>{" "}
-              pkt
-            </p>
-            <p>
-              4 &nbsp;&nbsp;{" "}
-              <b>
-                {Math.round((points * 75) / 100 + 1)} -{" "}
-                {Math.round((points * 85) / 100)}
-              </b>{" "}
-              pkt
-            </p>
-            <p>
-              4+ &nbsp;&nbsp;{" "}
-              <b>
-                {Math.round((points * 85) / 100)} -{" "}
-                {Math.round((points * 90) / 100 - 1)}
-              </b>{" "}
-              pkt
-            </p>
-            <p>
-              5- &nbsp;&nbsp;{" "}
-              <b>
-                {Math.round((points * 90) / 100)} -{" "}
-                {Math.round((points * 95) / 100)}
-              </b>{" "}
-              pkt
-            </p>
-            <p>
-              5 &nbsp;&nbsp;{" "}
-              <b>
-                {Math.round((points * 95) / 100 + 1)} - {Math.round(points)}
-              </b>{" "}
-              pkt
-            </p>
+            ))
+            }
+            
           </div>
           <div className="col">
             <p>
-              dop&nbsp;&nbsp; 30% - <b>{(points * 30) / 100}</b> pkt
+              dop&nbsp;&nbsp; 30% <b>{(points * 30) / 100}</b> pkt
             </p>
             <p>
-              dst&nbsp;&nbsp; 50% - <b>{(points * 50) / 100}</b> pkt
+              dst&nbsp;&nbsp; 50% <b>{(points * 50) / 100}</b> pkt
             </p>
             <p>
-              db&nbsp;&nbsp; 70% - <b>{(points * 70) / 100}</b> pkt
+              db&nbsp;&nbsp; 70% <b>{(points * 70) / 100}</b> pkt
             </p>
             <p>
-              bdb&nbsp;&nbsp; 90% - <b>{(points * 90) / 100}</b> pkt
+              bdb&nbsp;&nbsp; 90% <b>{(points * 90) / 100}</b> pkt
             </p>
             <p>
-              cel&nbsp;&nbsp; 100% - <b>{points}</b> pkt
+              cel&nbsp;&nbsp; 100% <b>{points}</b> pkt
             </p>
           </div>
         </div>
