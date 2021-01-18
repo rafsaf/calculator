f43b6058abe980dbde21fbba50db028cbb4e6244
rafsaf <rafal.safin12@gmail.com>
2021-01-18 23:26:57 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b853bd2c3862afeb8610.js"
+    "url": "webpack-runtime-6f9cc6397bba4cfe0751.js"
   },
   {
     "url": "framework-3e6dec6139dadaa7d0c7.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bcb3d732d3d4a81353c2397be98f8cde"
+    "revision": "b890991b563d75b75bd139f60a43cb91"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-19245c8506e49b502b12.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "8f7ef59a0804013362a845d48c21bcdc"
+    "revision": "810907f07656562398cecbbff8000416"
   },
   {
     "url": "polyfill-38cce18603b1d20cac17.js"
